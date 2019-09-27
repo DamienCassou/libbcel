d14e5f9a7dbeed7d015f646228c364689a9f2f54
Damien Cassou <damien@cassou.me>
2019-09-27 12:08:44 +0200

    * libbcel-client.el: Make it possible to POST and DELETE

--- libbcel-client.el
+++ libbcel-client.el
@@ -66,6 +66,40 @@ When CALLBACK is non-nil, evaluate it with the response."
                            (when callback
                              (funcall callback data))))))
 
+(defun libbcel-client--delete-url-from-token (access-token url &optional callback)
+  "Do a DELETE query to Basecamp 3 API at URL.
+
+ACCESS-TOKEN is found in the result of the OAUTH2 authentication.
+See `libbcel-oauth-get-access-token'.
+
+When CALLBACK is non-nil, evaluate it with the response."
+  (request
+   url
+   :type "DELETE"
+   :headers `(("User-Agent" . "bcel (damien@cassou.me)")
+              ("Authorization" . ,(format "Bearer %s" access-token)))
+   :parser #'json-read
+   :success (cl-function (lambda (&key data &allow-other-keys)
+                           (when callback
+                             (funcall callback data))))))
+
+(defun libbcel-client--post-url-from-token (access-token url &optional callback)
+  "Do a POST query to Basecamp 3 API at URL.
+
+ACCESS-TOKEN is found in the result of the OAUTH2 authentication.
+See `libbcel-oauth-get-access-token'.
+
+When CALLBACK is non-nil, evaluate it with the response."
+  (request
+   url
+   :type "POST"
+   :headers `(("User-Agent" . "bcel (damien@cassou.me)")
+              ("Authorization" . ,(format "Bearer %s" access-token)))
+   :parser #'json-read
+   :success (cl-function (lambda (&key data &allow-other-keys)
+                           (when callback
+                             (funcall callback data))))))
+
 (defun libbcel-client--get-path-from-token (access-token account-id path &optional callback)
   "Execute CALLBACK with the result of the GET call to PATH.
 
@@ -94,6 +128,20 @@ If PARAMS is non-nil it should be an alist that is passed to the GET request."
    (libbcel-client--oauth-store)
    (lambda (access-token)
      (libbcel-client--get-url-from-token access-token url callback params))))
+
+(defun libbcel-client-delete-url (url &optional callback)
+  "Do a DELETE request on URL and evaluate CALLBACK with the result."
+  (libbcel-oauth-get-access-token
+   (libbcel-client--oauth-store)
+   (lambda (access-token)
+     (libbcel-client--delete-url-from-token access-token url callback))))
+
+(defun libbcel-client-post-url (url &optional callback)
+  "Do a POST request on URL and evaluate CALLBACK with the result."
+  (libbcel-oauth-get-access-token
+   (libbcel-client--oauth-store)
+   (lambda (access-token)
+     (libbcel-client--post-url-from-token access-token url callback))))
 
 (provide 'libbcel-client)
 ;;; libbcel-client.el ends here
